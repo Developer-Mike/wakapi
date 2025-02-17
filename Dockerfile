9ed215269a71cbc99936e225cf7bac7337d6d40e
Developer-Mike <delta.cephei@outlook.com>
2025-02-17 13:26:53 +0100

    fix

--- Dockerfile
+++ Dockerfile
@@ -19,9 +19,6 @@ WORKDIR /app
 # Copy binary from build stage
 COPY --from=builder /app/wakapi /app/wakapi
 
-# Copy configuration file
-COPY config.yml /app/config.yml
-
 # Expose default port (adjust according to your config.yml)
 EXPOSE 3000
 
--- run-image.sh
+++ run-image.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 docker run -d \
-  -p 3000:3000 \
+  -p 443:3000 \
+  -v ~/wakapi/config.yml:/app/config.yml:ro \
+  -v ~/wakapi/origin-certificate.pem:/certs/origin-certificate.pem:ro \
+  -v ~/wakapi/private-key.pem:/certs/private-key.pem:ro \
   -v wakapi-data:/data \
   --name wakapi \
   --restart unless-stopped \
